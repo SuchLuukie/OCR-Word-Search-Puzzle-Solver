90d983ff1c803eab703423d127c252f9893de61c
SuchLuukie <62255292+SuchLuukie@users.noreply.github.com>
2022-07-04 20:19:53 +0200

    Segmenting of letters done

--- puzzle_input.py
+++ puzzle_input.py
@@ -1,7 +1,7 @@
 # Import libraries
-from PIL import Image
 import cv2
 
+# https://stackoverflow.com/questions/60515216/extracting-and-saving-characters-from-an-image
 
 class PuzzleInput:
     def __init__(self):
@@ -13,9 +13,33 @@ class PuzzleInput:
         self.extract_puzzle(self.gray)
 
 
-    # Extract the puzzle from 
+    # Extract the puzzle from image
     def extract_puzzle(self, img):
+        # Get the segments of the letters
         segments = self.extract_segments(img)
+
+        self.visualise(segments)
+
+        # Now determine which are letters and words
+        segments, words = self.extract_words(segments)
+        letters = self.extract_letters(segments)
+
+
+    # Extracts words and removes them from segments
+    def extract_words(self, segments):
+        words = []
+        return segments, words
+
+
+    # Extracts letters
+    def extract_letters(self, segments):
+        return
+
+
+    def get_image_from_segment(self, img, segment):
+        x, y, w, h = segment
+        image = img[y:y+h, x:x+w]
+        return image
         
 
     # Extract the segments of the letters in the image (Preparing for OCR)
@@ -23,28 +47,23 @@ class PuzzleInput:
         threshold = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)[1]
         contours = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
-        segments = []
-        for c in contours:
-            segment = cv2.boundingRect(c)
-            segments.append(segment)
+        # Get the bounding box of the contour
+        segments = [cv2.boundingRect(contour) for contour in contours]
 
-            # Box around the segment to show the letters
-            x,y,w,h = segment
-            cv2.rectangle(self.image, (x, y), (x + w, y + h), (36,255,12), 2)
+        # Reverse list for convenience
+        segments = list(reversed(segments))
 
-        cv2.imshow("image", self.image)
-        cv2.waitKey()
         return segments
 
-    # Extract the background colour of an image
-    def extract_background_colour(self, img):
-        # Get all the colours used in the image
-        colours = img.getcolors()
+    # Visualise the segments onto the image
+    def visualise(self, segments):
+        for idx, segment in enumerate(segments):
+            x, y, w, h = segment
+            cv2.rectangle(self.image, (x, y), (x + w, y + h), (36,255,12), 2)
+            cv2.putText(self.image, str(idx), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (36,255,12), 2)
+        cv2.imshow("img", self.image)
+        cv2.waitKey()
 
-        # The most used colour is the background colour
-        count, background_colour = max(colours)
-
-        return background_colour
 
 if __name__ == "__main__":
     PuzzleInput()
